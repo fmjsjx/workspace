6ee40d35bb72665618351f127f2de9c10da64f25
fmjsjx <fmjsjx@163.com>
2015-01-30 17:59:57 +0800

    Create resolve-links.sh

---
+++ resolve-links.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# resolve links - $0 may be a softlink
+PRG="$0"
+
+while [ -h "$PRG" ] ; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
+PRG_DIR=`dirname "$PRG"`
+PRG_DIR=`cd ${PRG_DIR}/..;pwd`
+cd ${PRG_DIR}
+
+# TODO
